d606a4b9cca245e2294a9b328897dfcc42c2bbd4
Caio Moraes <camoredev@gmail.com>
2022-02-15 00:21:50 -0300

    Exercício variaveis

---
+++ javaScript/fundamentos/bloco-02-JavaScript-para-iniciantes/dia-01-variables/script.js
@@ -0,0 +1,23 @@
+// Declarar uma variável com o seu nome
+
+var meuNome = "Caio Moraes";
+
+// Declarar uma variável com a sua idade
+
+var minhaIdade = 26;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = "Yakissoba";
+
+// Declarar 5 variáveis diferentes sem valores
+
+var corFavorita;
+var timeFavorito;
+var jogoFavorito; 
+var animalDeEstimacao;
+var estadoCivil;
