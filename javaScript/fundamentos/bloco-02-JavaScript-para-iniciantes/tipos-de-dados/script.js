826a36629651807ce069c9bbfa9fe50d11b524b1
Caio Moraes <camoredev@gmail.com>
2022-02-15 00:56:49 -0300

    Execício tipo de dados

---
+++ javaScript/fundamentos/bloco-02-JavaScript-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,24 @@
+// Declare uma variável contendo uma string
+
+let name = 'Caio Moraes'
+
+// Declare uma variável contendo um número dentro de uma string
+let number = '2022'
+// Declare uma variável com a sua idade
+
+let  idade = '26'
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+let meuNome = 'Caio';
+let sobrenome = 'Moraes';
+
+console.log(meuNome, sobrenome)
+// Coloque a seguinte frase em uma variável: It's time
+
+let frase = 'It\'s time'
+console.log(frase)
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof meuNome)
